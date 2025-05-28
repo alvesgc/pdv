f4f1f0548f69fd095809db37cfb1640c5b880f36
= <=>
2025-05-28 11:52:31 -0300

    feat: implement Layout component with SideBar and Header

---
+++ src/component/Layout.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import SideBar from "./SideBar";
+import Header from "./Header";
+
+const Layout = ({ children }) => {
+    const [isSidebarVisible, setSidebarVisible] = useState(false);
+    const toogleSideBar = () => {
+        setSidebarVisible(!isSidebarVisible);
+    }
+
+    return (
+     <div className="flex">
+        <SideBar visible={isSidebarVisible} />
+        <div className="flex-1">
+            <Header onClick={toogleSideBar}>
+                <main classNaame="p-6">{children}</main>
+            </Header>
+        </div>
+     </div>   
+    )
+}
+
+export default Layout;
