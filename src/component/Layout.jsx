cf22d847cce891a0bf75842519eb9ae22398f699
= <=>
2025-05-28 12:23:05 -0300

    style: format Layout component code for consistency

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -1,23 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import SideBar from "./SideBar";
 import Header from "./Header";
 
 const Layout = ({ children }) => {
-    const [isSidebarVisible, setSidebarVisible] = useState(false);
-    const toogleSideBar = () => {
-        setSidebarVisible(!isSidebarVisible);
-    }
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
 
-    return (
-     <div className="flex">
-        <SideBar visible={isSidebarVisible} />
-        <div className="flex-1">
-            <Header clickMenu={toogleSideBar}>
-                <main className="p-6">{children}</main>
-            </Header>
-        </div>
-     </div>   
-    )
-}
+  const toogleSideBar = () => {
+    setSidebarVisible(!isSidebarVisible);
+  };
+
+  const closeSideBar = () => {
+    setSidebarVisible(false);
+  }
+
+  return (
+    <div className="flex">
+      <SideBar visible={isSidebarVisible} onClose={closeSideBar} />
+      <div className="flex-1">
+        <Header clickMenu={toogleSideBar} />
+        <main className="p-6">{children}</main>
+      </div>
+    </div>
+  );
+};
 
 export default Layout;
