f9adb5be1cd29e8f7aaaed444f700818a9fac552
= <=>
2025-05-28 12:22:57 -0300

    style: format code in App component for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import Layout from './component/Layout';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Layout from "./component/Layout";
 
 export default function App() {
   return (
     <Router>
       <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
       </Layout>
     </Router>
   );
