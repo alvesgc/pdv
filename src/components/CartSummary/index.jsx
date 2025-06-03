1756719d063cefa968ae5eccda8e35e38f0a9c2e
Alisson <alissonalves64@hotmail.com>
2025-06-02 21:58:35 -0300

    feat: update CartSummary component for improved item count and total display

--- src/components/CartSummary/index.jsx
+++ src/components/CartSummary/index.jsx
@@ -1,11 +1,22 @@
-export default function CartSummary({ itemCount, total }) {
+import formatterPrice from "../../utils/formatters";
+
+export default function CartSummary({ itemCount, total, itemPrice }) {
   return (
     <div className="flex justify-between items-center gap-5 pt-4 border-t-2 border-gray-200">
       <p className="text-2xl font-bold text-right text-gray-900 mb-4">
         Itens: <span className="text-blue-600">{itemCount}</span>
       </p>
+      <p className="text-2xl font-bold text-right text-gray-900 mb-4">
+        Valor:{" "}
+        <span className="text-blue-600">
+          <formatterPrice value={itemPrice} />
+        </span>
+      </p>
       <h3 className="text-3xl font-bold text-right text-gray-900 mb-4">
-        Total: <span className="text-blue-600">R$ {total.toFixed(2)}</span>
+        Total:{" "}
+        <span className="text-blue-600">
+          <formatterPrice value={total} />
+        </span>
       </h3>
     </div>
   );
