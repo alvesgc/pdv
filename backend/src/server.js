9684c3706970a75854c23868789487bb16a14cb6
Alisson <alissonalves64@hotmail.com>
2025-06-04 17:08:22 -0300

    Refactor server.js to remove direct product handling and integrate product routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,33 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { PrismaClient } from '@prisma/client';
+import productRoutes from './routes/products.js';
+
 dotenv.config();
 
 const app = express();
-const prisma = new PrismaClient();
+app.use(cors());
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/products', async (req, res) => {
-  const products = await prisma.product.findMany()
-  res.json(products);
-})
-
-app.post('/products', async (req, res) => {
-  const {code, name, barcode, quantity, price, active  } = req.body;
-  const newProduct = await prisma.product.create({
-    data: {
-      code,
-      name,
-      barcode,
-      quantity,
-      price,
-      active
-    }
-  });
-  res.status(201).json(newProduct);
-})
+app.use('/products', productRoutes);
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server is running on port ${process.env.PORT || 3000}`));
