c04511f51473f1bbd549730048940aae0fe69208
Alisson <alissonalves64@hotmail.com>
2025-06-04 16:24:02 -0300

    Add server.js and update Product model to include active field

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+dotenv.config();
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/products', async (req, res) => {
+  const products = await prisma.products.findMany()
+  res.json(products);
+})
+
+app.post('/products', async (req, res) => {
+  const {name, price, quantity, active } = req.body;
+  const newProduct = await prisma.products.create({
+    data: {
+      name,
+      price,
+      quantity,
+      active
+    }
+  });
+  res.status(201).json(newProduct);
+})
+
+app.listen(process.env.PORT || 3000, () => console.log(`Server is running on port ${process.env.PORT || 3000}`));
