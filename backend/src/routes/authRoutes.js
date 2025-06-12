255f15e6a1254d45d587b6434de93175bb0d5d61
= <=>
2025-06-12 12:59:38 -0300

    feat: implement login and registration functionality with JWT authentication

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,17 +1,10 @@
 import express from "express";
-<<<<<<< HEAD
 import { login } from "../controllers/auth/loginController.js";
 import { register } from "../controllers/auth/registerController.js";
-=======
-import { login } from "../controllers/auth/authController.js";
->>>>>>> 9dbf01a (feat: implement authentication with login and JWT token generation)
 
 const router = express.Router();
 
 router.post("/login", login);
 
-<<<<<<< HEAD
 router.post('/register', register);
-=======
->>>>>>> 9dbf01a (feat: implement authentication with login and JWT token generation)
 export default router;
