c1f5858197718aa662b45b5b0a3bcdc29a24d51e
= <=>
2025-06-12 10:53:12 -0300

    feat: initialize Prisma client and set up API routes in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,17 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
-import productRoutes from './routes/products.js';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 
-app.use('/products', productRoutes);
-const PORT = process.env.PORT || 3000;
-app.listen(PORT,() => console.log(`Server is running on port ${process.env.PORT || 3000}`));
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
