90c6483c1f19e8a2ec8174ac7e0e4105bfda6de7
Alisson <alissonalves64@hotmail.com>
2025-06-14 10:14:39 -0300

    feat: add bcrypt and react-jwt dependencies; update package-lock.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Log from "./pages/Log";
 import Layout from "./components/Layout";
+// import Stock from "./pages/Stock";
 import "./index.css";
 
 export default function App() {
@@ -12,7 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
-          <Route path="/Stock" element={<Stock />} />
+          {/* <Route path="/Stock" element={<Stock />} /> */}
           <Route path="/Log" element={<Log />} />
         </Routes>
       </Layout>
