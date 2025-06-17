48ebef7552c81914d0c3cbcd0e233a52dca821ae
= <=>
2025-06-17 12:34:58 -0300

    feat: add PaymentType model and migration for Product code constraint

---
+++ backend/prisma/migrations/20250617150937_payment_type/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[code]` on the table `Product` will be added. If there are existing duplicate values, this will fail.
+  - Made the column `code` on table `Product` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" ALTER COLUMN "code" SET NOT NULL;
+
+-- CreateTable
+CREATE TABLE "PaymentType" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "clientId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "PaymentType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_code_key" ON "Product"("code");
+
+-- AddForeignKey
+ALTER TABLE "PaymentType" ADD CONSTRAINT "PaymentType_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
