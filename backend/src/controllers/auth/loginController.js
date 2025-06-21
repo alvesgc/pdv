7f95a6b49f6478c05d5cc466be1894cf41e4a446
Alisson <alissonalves64@hotmail.com>
2025-06-21 16:22:34 -0300

    feat: integrate Supabase for user registration; handle user creation and error management

--- backend/src/controllers/auth/loginController.js
+++ backend/src/controllers/auth/loginController.js
@@ -1,31 +1,31 @@
-import prisma from "../../../prisma/client.js";
-import { comparePassword } from "../../utils/hash.js";
-import jwt from "jsonwebtoken";
+// import prisma from "../../../prisma/client.js";
+// import { comparePassword } from "../../utils/hash.js";
+// import jwt from "jsonwebtoken";
 
-export const login = async (req, res) => {
-  const { email, password } = req.body;
+// export const login = async (req, res) => {
+//   const { email, password } = req.body;
 
-  if (!email || !password)
-    return res.status(400).json({ message: "Preencha todos os campos." });
+//   if (!email || !password)
+//     return res.status(400).json({ message: "Preencha todos os campos." });
 
-  const user = await prisma.user.findUnique({ where: { email } });
+//   const user = await prisma.user.findUnique({ where: { email } });
 
-  if (!user)
-    return res.status(400).json({ message: "Email ou senha incorretos." });
+//   if (!user)
+//     return res.status(400).json({ message: "Email ou senha incorretos." });
 
-  const isMatch = await comparePassword(password, user.password);
-  if (!isMatch)
-    return res.status(400).json({ message: "Email ou senha incorretos." });
+//   const isMatch = await comparePassword(password, user.password);
+//   if (!isMatch)
+//     return res.status(400).json({ message: "Email ou senha incorretos." });
 
-  const token = jwt.sign(
-    {
-      userId: user.id,
-      clientId: user.clientId,
-      email: user.email,
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" }
-  );
+//   const token = jwt.sign(
+//     {
+//       userId: user.id,
+//       clientId: user.clientId,
+//       email: user.email,
+//     },
+//     process.env.JWT_SECRET,
+//     { expiresIn: "1d" }
+//   );
 
-  return res.json({ token });
-};
+//   return res.json({ token });
+// };
